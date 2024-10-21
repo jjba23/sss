39d1681c345a327eae66d2d7b5395bc70b2f09dd
Josep Bigorra <jjbigorra@gmail.com>
2024-10-21 13:54:27 +0200

    conkFine words and an insinuating appearance are seldom associated with true virtue.

--- home/joe/stumpwm/init.lisp
+++ home/joe/stumpwm/init.lisp
@@ -125,7 +125,7 @@
     ;;(display "\n>>=  start LXSession...\n")
     (run-shell-command "lxsession --de=StumpWM &")
     ;;(display "\n>>=  start Conky...\n")
-    (run-shell-command "conky &")
+    ;;(run-shell-command "conky &")
     ))
 
 (setf *mouse-focus-policy*    :click
